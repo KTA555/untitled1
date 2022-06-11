117430afc230bdfc310e4176642c306e22142be4
islam <islam05_15@mail.ru>
2022-06-11 11:14:19 +0300

    ok

---
+++ src/Token.java
@@ -0,0 +1,11 @@
+public class Token {
+    TokenType type;
+    String value;
+    int pos;
+
+    public Token(TokenType type, String value, int pos) {
+        this.type = type;
+        this.value = value;
+        this.pos = pos;
+    }
+}
